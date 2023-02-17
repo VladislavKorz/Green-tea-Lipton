c309337ab368d88ffa38618a4705fe3786a827eb
alex123321-maker <alexander12110@gmail.com>
2023-02-18 05:11:56 +0700

    Добавлен профиль для user

--- sites/users/models.py
+++ sites/users/models.py
@@ -1,26 +1,30 @@
-# from django.db import models
-# from django.contrib.auth.models import User
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-# class Profile(models.Model):
-#     ROLS_CHOICES = (
-#         ("NC", "Стажёр"),
-#         ("HR", "HR"),
-#         ("DIR", "Стажёр"),
+class Profile(models.Model):
+    ROLS_CHOICES = (
+        ("NC", "Стажёр"),
+        ("HR", "HR"),
+        ("DIR", "Руководитель"),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rols = models.CharField(max_length=10, choices=ROLS_CHOICES, default="NC")
+    phone = models.CharField(max_length=20, default='-')
+    telegram_id = models.CharField(max_length=50, default='-')
 
-#     )
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     rols = models.CharField(max_length=9, choices=ROLS_CHOICES, default="NC")
+    class Meta:
+        verbose_name_plural = 'Профили'
+        verbose_name = 'Профиль'
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
 
-# @receiver(post_save, sender=User)
-# def create_user_profile(sender, instance, created, **kwargs):
-#     if created:
-#         Profile.objects.create(user=instance)
-
-
-# @receiver(post_save, sender=User)
-# def save_user_profile(sender, instance, **kwargs):
-#     instance.profile.save()
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
