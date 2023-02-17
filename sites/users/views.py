25042f9788fbdd1979b65295dc6501f47b81c7c3
alex123321-maker <alexander12110@gmail.com>
2023-02-18 01:22:04 +0700

    132

--- sites/users/views.py
+++ sites/users/views.py
@@ -8,7 +8,7 @@ from django.views import View
 class LoginView(View):
     def get(self, request, *args, **kwargs):
         form = AuthenticationForm()
-        return render(request, 'login.html', {'form': form})
+        return render(request, 'users/login.html', {'form': form})
 
     def post(self, request, *args, **kwargs):
         form = AuthenticationForm(data=request.POST)
@@ -18,7 +18,7 @@ class LoginView(View):
                                     )
             login(request, new_user)
         else:
-            return render(request, 'login.html', {'form': form})
+            return render(request, 'users/login.html', {'form': form})
 
         return redirect('account')
 
