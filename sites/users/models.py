51aaa13ed455cd507968ac55d150e5f675fa338a
Minervaa45 <102596515+Minervaa45@users.noreply.github.com>
2023-02-18 23:24:30 +0700

    xfxix

--- sites/users/models.py
+++ sites/users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from tasks.models import Task
 
 class Department(models.Model):
     title = models.CharField("Название", max_length=50)
