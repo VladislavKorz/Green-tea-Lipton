9108feae77305531144eb8d292b9ce3ee4f7ffb0
Minervaa45 <102596515+Minervaa45@users.noreply.github.com>
2023-02-19 03:20:45 +0700

    fix pofil

--- sites/achievements/models.py
+++ sites/achievements/models.py
@@ -1,20 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django_ckeditor_5.fields import CKEditor5Field
 
 class Achievement(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = CKEditor5Field('Описание',null=True)
     image = models.ImageField(upload_to='achievements/')
-    conditions = models.TextField()
+    conditions = CKEditor5Field('Коментарий',null=True)
     date_earned = models.DateTimeField(null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='achievements')
 
+
+    def get_photo(self):
+        if self.image:
+            return self.image.url
+        else:
+            return '/static/images/profile-img.jpg'
+        
     def __str__(self):
         return self.title
 
 class UserAchievement(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_achievements')
+    user = models.ForeignKey("users.Profile", on_delete=models.CASCADE, related_name='user_achievements')
     achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE, related_name='achievement_users')
     date_earned = models.DateTimeField(auto_now_add=True)
 
@@ -22,13 +29,4 @@ class UserAchievement(models.Model):
         unique_together = ('user', 'achievement')
 
     def __str__(self):
-        return f'{self.user.username} - {self.achievement.title}'
-
-class UserAchievementProgress(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='achievement_progress')
-    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE, related_name='user_progress')
-    date_completed = models.DateTimeField(auto_now_add=True)
-    
-
-    def __str__(self):
-        return f'{self.user.username} - {self.achievement.title}'
+        return f'{self.user} - {self.achievement.title}'
