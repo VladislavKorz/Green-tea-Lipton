c9c3c872c400252f7f521edb839f957de9969cc0
Minervaa45 <102596515+Minervaa45@users.noreply.github.com>
2023-02-18 17:14:07 +0700

    add tasks

--- sites/sites/urls.py
+++ sites/sites/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('account/', include('users.urls')),
     path('road-maps/', include('roadMaps.urls')),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
+    path('tasks/', include('tasks.urls')),
     
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
