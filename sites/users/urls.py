6f679254172c770902a54dcc394cc6c8d340a674
Minervaa45 <102596515+Minervaa45@users.noreply.github.com>
2023-02-19 06:04:42 +0700

    add setttings

--- sites/users/urls.py
+++ sites/users/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('contacts/', ContactsView , name="contacts"),
     path('login/', LoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
+    path('settings/', SettingsView, name="settings"),
     path('sync=<str:user_id> ', sync, name='sync'),
+    
 ]
