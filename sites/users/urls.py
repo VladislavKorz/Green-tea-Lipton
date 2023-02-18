b77ea5aa0d261b0d9cdd89561faea986fbca760f
VladislavKorz <korzhov.vladd@gmail.com>
2023-02-18 15:57:29 +0700

    fix

--- sites/users/urls.py
+++ sites/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from users.views import *
 
 urlpatterns = [
-    path('', AccountView , name="account"),
-    path('login/', LoginView.as_view(), name="login-form"),
+    path('', AccountView , name="profile"),
+    path('login/', LoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
 ]
