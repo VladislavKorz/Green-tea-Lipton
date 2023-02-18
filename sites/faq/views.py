45e0b89d41dae2b7abb4f7019966e21abf01702d
Deni070 <74984564+Deni070@users.noreply.github.com>
2023-02-18 23:14:26 +0700

    fix

--- sites/faq/views.py
+++ sites/faq/views.py
@@ -4,5 +4,4 @@ from .models import FAQ
 
 def faq(request, pk):
     faq = get_object_or_404(FAQ, pk=pk)
-    
     return render(request, 'faq/faq_list.html', {'objlist': FAQ.objects.all()})
--- sites/sites/urls.py
+++ sites/sites/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path("ckeditor5/", include('django_ckeditor_5.urls')),
     path('tasks/', include('tasks.urls')),
     path('ads/', include('ads.urls') ),
+    path('faq/', include('faq.urls')),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
